fdf56c4877b3da615dbf02d67c8dbbe400b8ec36
Manish Kumar <manishjanky@gmail.com>
2023-03-24 20:42:24 +0530

    audio volume

--- src/app/components/audio-player/audio-player.component.ts
+++ src/app/components/audio-player/audio-player.component.ts
@@ -7,21 +7,27 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class AudioPlayerComponent implements OnInit {
   @ViewChild('audioref') audioRef: ElementRef;
+  @ViewChild('volumeRange') volumeRange: ElementRef;
   playedTime: string;
   isPlaying: boolean;
   history: any[] = [];
   audio: any;
+  volume: number = 8;
   constructor() {}
   get duration(): string {
-    return (
-      (this.audioElement?.duration / 60).toFixed(2).replace('.', ':') || '0:00'
-    );
+    const duration = this.audioElement?.duration / 60;
+    return !isNaN(duration) ? duration.toFixed(2).replace('.', ':') : '0:00';
   }
   get image() {
     return this.audio?.image || '/assets/images/audio_player.jpg';
   }
   get audioElement(): HTMLAudioElement {
     return this.audioRef?.nativeElement;
+  }
+
+  get activeVolume() {
+    // alert(`${this.volumeRange?.nativeElement?.width * (this.volume / 10) - 42}px`);
+    return `${this.volumeRange?.nativeElement.clientWidth * (this.volume / 10) - 2}px`;
   }
   ngOnInit(): void {}
 
@@ -33,5 +39,9 @@ export class AudioPlayerComponent implements OnInit {
 
   playPause() {
     this.isPlaying ? this.audioElement.pause() : this.audioElement.play();
+  }
+
+  volumeChange() {
+    this.audioElement.volume = this.volume / 10;
   }
 }
