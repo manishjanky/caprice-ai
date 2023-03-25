713b1d865067ffd2a018d726e8763468d20bc3fe
Manish Kumar <manishjanky@gmail.com>
2023-03-25 11:09:47 +0530

    Hand gestures recognize..

--- src/app/services/music.service.ts
+++ src/app/services/music.service.ts
@@ -1,17 +1,25 @@
+import { MUSIC_PLAYING } from './../utils/music.utils';
 import { SAAVAN_API } from './../utils/api.utils';
 import { environment } from './../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MusicService {
   saavanUrl = environment.saavanUrl;
+  musicPlaying = new EventEmitter<MUSIC_PLAYING>();
   constructor(private http: HttpClient) {}
   searchSong(text: string, page = 1) {
     return this.http.get(
       `${this.saavanUrl}${SAAVAN_API.searchSongs}?query=${text}&page=${page}`
     );
+  }
+  startPlayingMusic() {
+    this.musicPlaying.emit(MUSIC_PLAYING.Start);
+  }
+  sopPlayingMusic(reason: MUSIC_PLAYING) {
+    this.musicPlaying.emit(reason);
   }
 }
--- src/app/utils/music.utils.ts
+++ src/app/utils/music.utils.ts
@@ -4,6 +4,10 @@ export enum MUSIC_COMMANDS {
   next = 'next',
   previous = 'previous',
 }
-
+export enum MUSIC_PLAYING {
+  Start = 'START',
+  Pause = 'PAUSE',
+  Stop = 'STOP',
+}
 export const MUSIC_GRAMMAR =
   '#JSGF V1.0; grammar music commands; public <music commands> = play | pause | next | previour ;';
